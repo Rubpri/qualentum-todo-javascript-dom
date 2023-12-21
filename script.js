d7283b2fbec4518076e5d3867b9389fee51d0d5c
Rubpri <rubenprietoserrano@gmail.com>
2023-12-21 13:32:13 +0100

    version 2

--- script.js
+++ script.js
@@ -2,43 +2,64 @@ const input = document.querySelector("input");
 const addTaskButton = document.querySelector(".btn");
 const ul = document.querySelector("ul");
 
-addTaskButton.addEventListener("click", (e) => {
-    
+const tasksArray = [];
+const tasksInfo = [];
+let id = 0;
+
+addTaskButton.addEventListener("click", () => {
+
     const task = input.value;
 
-    const li = document.createElement("li");
-    
-    const article = document.createElement("article");
-    
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.className = "task-checkbox";
-    
-    const span = document.createElement("span");
-    span.textContent = task;
-    span.className = "task-text";
+    if (task != "") {
 
-    ul.appendChild(li);
-    ul.className = "task-list";
-    
-    li.appendChild(article);
-    article.appendChild(checkbox);
-    article.appendChild(span);
-    li.appendChild(deleteTask());
+        tasksArray.push(task)
+        
+        tasksInfo.push(
+            {
+                "id": id,
+                "nombre": task,
+                "completada": false
+            }
+        )  
 
-    function deleteTask() {
-        const trash = document.createElement("i");
-        trash.className = "fa fa-trash";
+        id += 1;
+        
+        const li = document.createElement("li");
+        const article = document.createElement("article");
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.className = "task-checkbox";
+        const span = document.createElement("span");
+        span.textContent = task;
+        span.className = "task-text";
 
-        trash.addEventListener("click", (e) => {
-            const task = e.target.parentElement;
-            ul.removeChild(task)
-        })
+        ul.appendChild(li);
+        ul.className = "task-list";
+        li.appendChild(article);
+        article.appendChild(checkbox);
+        article.appendChild(span);
 
-        return trash;
-    }
+        
+       
+        function deleteTask() {
+            const trash = document.createElement("i");
+            trash.className = "fa fa-trash";
 
-    input.value = "";
-    
+            trash.addEventListener("click", (e) => {
+                const task = e.target.parentElement;
+                ul.removeChild(task)
+            })
+
+            return trash;
+        }
+
+        li.appendChild(deleteTask());
+
+        input.value = "";
+
+        
+        console.log(tasksInfo)
+        console.log(tasksArray)
+       
+    }  
 })
-
